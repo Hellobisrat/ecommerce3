5f3d26632d078b8a8e7f915d5b15b01af9581bc5
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-04-22 19:07:18 -0500

    user model controller and middleware added

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import authRoutes from "./routes/authRoutes.js";
+
+
 
 dotenv.config();
 
@@ -8,7 +11,7 @@ const app=express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get('/api/health',(req,res)=>{
   res.json({status:'OK'})
 })
