9233b86d333f0a101cd104ab6ff1fc427399374b
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-04-22 19:16:03 -0500

    product model, controller and route added

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+
 
 
 
@@ -12,6 +14,7 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 app.get('/api/health',(req,res)=>{
   res.json({status:'OK'})
 })
